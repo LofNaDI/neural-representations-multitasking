8ba8a52f27cc51ef9ca933c1dfb4f0e80b3c5a7b
Santiago Galella <santi.galella@gmail.com>
2024-01-27 17:32:13 +0100

    Update representations module task switching

--- multitask/multitask/models/task_switching/__init__.py
+++ multitask/multitask/models/task_switching/__init__.py
@@ -4,10 +4,11 @@ from .representations import calculate_rdm, plot_rdm
 from .utils import test, train
 
 __all__ = [
-    "get_layer_activations",
-    "get_task_model",
-    "calculate_rdm",
-    "plot_rdm",
-    "train",
-    "test",
+    'get_layer_activations',
+    'get_task_model',
+    'get_mean_activations'
+    'calculate_rdm',
+    'plot_rdm',
+    'train',
+    'test',
 ]
--- multitask/multitask/models/task_switching/representations.py
+++ multitask/multitask/models/task_switching/representations.py
@@ -6,92 +6,95 @@ for task-swithing networks.
 - calculate_rdm
 - plot_rdm
 """
+import copy
+
 import numpy as np
 import seaborn as sns
 
 
 def get_mean_activations(activations,
-                         names_tasks,
                          num_hidden,
-                         list_numbers,
-                         num_classes):
+                         list_labels,
+                         tasks_names):
     """
-    Calculates the mean_activation matrix per layer.
+    Calculates the mean activation of units by class per layer.
 
     Args:
         activations (dict): Dictionary of np.ndarray containing the matrix
             of activations (N_test x N_hidden_units).
-        names_tasks (list): Names of tasks.
         num_hidden (list): Number of hidden units per layer.
-        list_numbers (list): List of numbers of test set (N_test, ).
+        list_labels (list): List of numbers of test set (N_test, ).
+        tasks_names (list): List of tasks nmes (N_tasks, ).
 
     Returns:
-        dict: Mean of activations per layer.
+        dict: Mean of activations of units by class per layer.
     """
     mean_activations = {}
-    for name_task in names_tasks:
+
+    activations_tmp = copy.deepcopy(activations)
+    list_labels_tmp = copy.deepcopy(list_labels)
+
+    if isinstance(activations_tmp, dict):
+        assert list(activations_tmp.keys()) == tasks_names
+        activations_tmp = list(activations_tmp.values())
+
+    if isinstance(list_labels_tmp, np.ndarray):
+        num_tasks = len(tasks_names)
+        list_labels_tmp = [list_labels_tmp for _ in range(num_tasks)]
+
+    for name_task, activation_task, labels_task in zip(tasks_names,
+                                                       activations_tmp,
+                                                       list_labels_tmp):
         mean_activations[name_task] = {}
+        num_classes = len(set(labels_task))
+
         for i_layer, num_units in enumerate(num_hidden, 1):
-            layer_name = f"layer{i_layer}"
+            layer_name = f'layer{i_layer}'
             mean_activations[name_task][layer_name] = \
                 np.zeros((num_classes, num_units))
-            for number in range(num_classes):
-                idxs_activations = list_numbers == number
-                activations_number = activations[name_task][layer_name][
-                    idxs_activations, :
-                ]
-                mean_activations[name_task][layer_name][number, :] = np.mean(
-                    activations_number, axis=0
-                )
+
+            for label in range(num_classes):
+                activations_label = \
+                    activation_task[layer_name][labels_task == label, :]
+                mean_activations[name_task][layer_name][label, :] = \
+                    np.mean(activations_label, axis=0)
 
     return mean_activations
 
 
-def calculate_rdm(activations,
-                  test_tasks,
-                  num_hidden,
-                  list_numbers,
-                  num_classes=None):
+def calculate_rdm(mean_activations,
+                  tasks_names):
     """
     Calculates the Representational Dissimilarity Matrix (RDM) per layer.
 
     Args:
-        activations_tasks (list): List of dictionaries of activations per task.
-        test_tasks (dict): Dictionary of test tasks.
-        num_hidden (list): Number of hidden units per layer.
-        list_numbers (list): List of numbers of test set (N_test, ).
+        mean_activations (dict): Mean of activations per layer.
+        tasks_names (list): List of tasks nmes (N_tasks, ).
 
     Returns:
         dict: Dictionary of RDMs per layer.
     """
-    names_tasks = list(test_tasks.keys())
-
-    if num_classes is None:
-        num_classes = len(set(list_numbers))
-
-    mean_activations = get_mean_activations(activations,
-                                            names_tasks,
-                                            num_hidden,
-                                            list_numbers,
-                                            num_classes)
     rdm_dict = {}
+    num_layers = len(mean_activations[tasks_names[0]])
 
-    for i_layer, _ in enumerate(num_hidden, 1):
+    for i_layer in range(1, num_layers+1):
         layer_name = f"layer{i_layer}"
 
         mean_activations_layer = None
-        for name_task in names_tasks:
+        for name_task in tasks_names:
             if mean_activations_layer is None:
                 mean_activations_layer = \
                     mean_activations[name_task][layer_name]
             else:
                 mean_activations_layer = np.vstack(
-                    (mean_activations_layer,
-                     mean_activations[name_task][layer_name])
+                    (
+                        mean_activations_layer,
+                        mean_activations[name_task][layer_name],
+                    )
                 )
 
-        correlation_dm = np.corrcoef(mean_activations_layer, rowvar=True)
-        rdm_dict[i_layer] = (1 - correlation_dm) / 2
+        correlelation_rdm = np.corrcoef(mean_activations_layer, rowvar=True)
+        rdm_dict[i_layer] = (1 - correlelation_rdm) / 2
 
     return rdm_dict
 
